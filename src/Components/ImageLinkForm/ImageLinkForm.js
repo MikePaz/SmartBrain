255f54bcdb45395144c0c6e1c603572c894ef723
Mike Pazarlis <mike.pazarlis@gmail.com>
2019-06-15 20:20:31 +0300

    Commiting Components

---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ImageLinkForm = () => {
+    return (<div>
+        <p className = 'f3'>
+            {'Detect faces in your pictures'}
+        </p>
+    </div>)
+}
+
+export default ImageLinkForm
