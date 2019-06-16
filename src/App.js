61b72c397b95caeee00c1b699b05ef2165d7aa04
Mike Pazarlis <mike.pazarlis@gmail.com>
2019-06-16 21:07:04 +0300

    Adding more Components and Style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank'
+import Particles from 'react-particles-js'
 
+const particleOptions = {
+  particles : {
+   number: {
+      value:300,
+      density:{
+        eanble:true,
+        value_area:800
+      }
+   } 
+}
+}
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particleOptions} />
       <Navigation/>
        <Logo/>
-      {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+       <Rank/>
+       <ImageLinkForm/>
+      {/* <FaceRecognition/>  */}
     </div>
   );
 }
