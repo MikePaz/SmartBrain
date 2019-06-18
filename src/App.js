6b1a848dfc26f8c102cc5555dd6d24643f1eac20
Mike Pazarlis <mike.pazarlis@gmail.com>
2019-06-18 17:10:37 +0300

    Profile update + Increasing Rank Count based on detections

--- src/App.js
+++ src/App.js
@@ -87,18 +87,32 @@ class App extends Component
     this.setState({input : event.target.value});
   }
 
-  onButtonSubmit = () =>{
-    this.setState({imageUrl:this.state.input})
-
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
     app.models
-    .predict(
-    Clarifai.FACE_DETECT_MODEL,
-        this.state.input
-    )
-    .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err=> console.log(err))
-}
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
 
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+    
 onRouteChange = (route) =>{
   if(route==='signout') {
     this.setState({isSignedIn:false})
