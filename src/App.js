7046380328cac5c3014af9bc5477073ea80fa0e5
Mike Pazarlis <mike.pazarlis@gmail.com>
2019-06-26 17:43:36 +0300

    Adding initial State

--- src/App.js
+++ src/App.js
@@ -30,27 +30,30 @@ const particleOptions = {
 }
 }
 
+const initialState = {
+  
+    input : '' ,
+    imageUrl:'',
+    box:{},
+    route:'SignIn',
+    isSignedIn: false,
+    
+    user:{
+      id:'',
+      name:'',
+      email:'',
+      password:'',
+      entries:0,
+      joined:''
+    }
+  
+}
 
 class App extends Component 
   {
   constructor() {
     super();
-    this.state = {
-      input : '' ,
-      imageUrl:'',
-      box:{},
-      route:'SignIn',
-      isSignedIn: false,
-      
-      user:{
-        id:'',
-        name:'',
-        email:'',
-        password:'',
-        entries:0,
-        joined:''
-      }
-    }
+    this.state = initialState
   }
 
   loadUser =(data)=>{
@@ -106,6 +109,7 @@ class App extends Component
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
+            .catch(console.log)
 
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
@@ -115,7 +119,7 @@ class App extends Component
     
 onRouteChange = (route) =>{
   if(route==='signout') {
-    this.setState({isSignedIn:false})
+    this.setState({initialState})
   } else if( route ==='home') {
     this.setState({isSignedIn:true})
   }
